ba60dbbcb8bd372c10d67cb5f8d74be60c2018f2
Constantine <862947199@qq.com>
2026-06-12 02:10:55 +0800

    Release v0.0.3

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -2,7 +2,7 @@ package buildinfo
 
 import "runtime"
 
-var Version = "0.0.2"
+var Version = "0.0.3"
 
 func UserAgent() string {
 	return "agnes-cli/" + Version + " (" + runtime.GOOS + "/" + runtime.GOARCH + ")"
